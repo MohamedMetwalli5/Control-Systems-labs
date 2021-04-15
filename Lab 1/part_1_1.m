b9cfd804a40b3a8e0fde55f7f23d9be6aefcb189
Abdallah <abdallah.yossf@gmail.com>
2021-04-15 02:03:21 +0200

    Lab 1, Part I, Exercise 1

---
+++ Lab 1/part_1_1.m
@@ -0,0 +1,22 @@
+clc;
+A = magic(6);
+
+fprintf('1) A = \n')
+disp(A)
+
+fprintf('fourth row = \n')
+disp(A(4, :));
+
+fprintf('--------------------------\n');
+x = (0:0.1:1.1);
+y = (10:21);
+fprintf("2) x*y:\n")
+disp(x.*y)
+fprintf("y/x:\n")
+disp(y./x)
+
+fprintf('--------------------------\n');
+r = randi([-8, 9], [4, 5]);
+fprintf('3)')
+disp(r)
+
