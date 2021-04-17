ff6e5bb810c4a92d982248c01558dd7cc65af1f9
ABE-Mark45 <56171892+ABE-Mark45@users.noreply.github.com>
2021-04-17 04:30:02 +0200

    Update part_3_2.m

--- Lab 1/Part III/part_3_2.m
+++ Lab 1/Part III/part_3_2.m
@@ -9,8 +9,9 @@ theta = 0;
 zeta = 3 / (2 * sqrt(2));
 y_1 = y(y0, zeta, omega, t, theta);
 
-subplot(2,1,1)
+subplot(2,1,1);
 plot(t,y_1);
+title('y_0=0.15, \omega_n=\surd{2}, \zeta= 3 /(2\surd{2}), \theta=0');
 
 % Case 2:
 zeta = 1 / (2 * sqrt(2));
@@ -18,4 +19,4 @@ y_2 = y(y0, zeta, omega, t, theta);
 
 subplot(2,1,2)
 plot(t,y_2);
-
+title('y_0=0.15, \omega_n=\surd{2}, \zeta= 1 /(2\surd{2}), \theta=0');
