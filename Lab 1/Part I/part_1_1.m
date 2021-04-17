50d281cf162d0d69add1dc145b3fdf83168568bf
ABE-Mark45 <56171892+ABE-Mark45@users.noreply.github.com>
2021-04-17 02:13:28 +0200

    Update part_1_1.m

--- Lab 1/Part I/part_1_1.m
+++ Lab 1/Part I/part_1_1.m
@@ -10,14 +10,17 @@ disp(A(4, :));
 
 fprintf('--------------------------\n');
 x = (0:0.1:1.1);
+fprintf("x = \n");
+disp(x);
 y = (10:21);
-fprintf("2) x*y:\n")
+fprintf("y = \n");
+disp(y);
+fprintf("2) x*y = \n")
 disp(x.*y)
-fprintf("y/x:\n")
+fprintf("y/x = \n")
 disp(y./x)
 
 fprintf('--------------------------\n');
 r = randi([-8, 9], [4, 5]);
 fprintf('3)')
 disp(r)
-
