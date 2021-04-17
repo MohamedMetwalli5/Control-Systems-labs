4dfa7eb9d6b4e191bc7af83b2768e72cb8835aca
ABE-Mark45 <56171892+ABE-Mark45@users.noreply.github.com>
2021-04-17 04:59:06 +0200

    Update part_3_3.m

--- Lab 1/Part III/part_3_3.m
+++ Lab 1/Part III/part_3_3.m
@@ -3,7 +3,7 @@ clear;
 
 
 tf = 0:10:200;
-tc = 0:10:200;
+tc = zeros(size(tf,2),1);
 for i = 1:size(tf,2)
     tc(i) = c_to_f(tf(i));
 end
@@ -12,7 +12,7 @@ disp(tc)
 plot(tf,tc)
 xlabel("Celsius")
 ylabel("Fahrenheit")
-text(100,212,'\leftarrow (100°C, 212°F)Water Boiling')
+text(100,212,'\leftarrow (100Â°C, 212Â°F)Water Boiling')
 
 
 function [f] = c_to_f (c)
